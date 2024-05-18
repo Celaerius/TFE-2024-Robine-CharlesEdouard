0e5410ef681295d12274938e20bcbfb086231ded
Celaerius <robinecharlesedouard@gmail.com>
2024-05-18 19:21:40 +0200

    LoginScreenUpdated

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -10,8 +10,13 @@ import {
   Platform,
   ScrollView,
 } from "react-native";
-import Animated, { FadeInDown, FadeInUp } from "react-native-reanimated";
+import Animated, {
+  FadeInDown,
+  FadeInUp,
+  FadeIn,
+} from "react-native-reanimated";
 import { useNavigation } from "@react-navigation/native";
+import LottieView from "lottie-react-native";
 
 export default function LoginScreen() {
   const [email, setEmail] = useState("");
@@ -30,67 +35,100 @@ export default function LoginScreen() {
         backgroundColor={"transparent"}
         translucent={true}
       />
-      <Image
-        style={styles.bgImage}
-        source={require("../../assets/background.png")}
-      />
-      <View style={styles.lampContainer}>
-        <Animated.Image
-          entering={FadeInUp.delay(200).duration(1000).springify()}
-          style={styles.lamp1}
-          source={require("../../assets/light.png")}
+      <Animated.View entering={FadeIn.duration(1000)}>
+        <LottieView
+          position="absolute"
+          height={300}
+          width={700}
+          left={-100}
+          top={0}
+          style={styles.bgImage}
+          source={require("../../assets/animations/BackgrounLogin.json")}
+          autoPlay
+          keepLastFrame={true}
         />
-        <Animated.Image
-          entering={FadeInUp.delay(400).duration(1000).springify()}
-          style={styles.lamp2}
-          source={require("../../assets/light.png")}
+      </Animated.View>
+      <Animated.View entering={FadeInUp.duration(1000).delay(500).springify()}>
+        <LottieView
+          height={300}
+          width={300}
+          position="absolute"
+          left={50}
+          source={require("../../assets/animations/mascot1.json")}
+          autoPlay
+          loop
         />
-      </View>
+      </Animated.View>
       <View style={styles.formContainer}>
-        <View style={styles.formTitle}>
-          <Animated.Text
-            entering={FadeInUp.duration(1000).springify()}
-            style={styles.title}
-          >
-            Login
-          </Animated.Text>
-        </View>
-        <View style={styles.formInputContainer}>
-          <Animated.View
-            entering={FadeInDown.duration(1000).delay(200).springify()}
-            style={styles.formInput}
-          >
-            <TextInput
-              placeholder="Email"
-              placeholderTextColor={"gray"}
-              value={email}
-              onChangeText={setEmail}
-            />
-          </Animated.View>
-          <Animated.View
-            entering={FadeInDown.duration(1000).delay(400).springify()}
-            style={[styles.formInput, { marginBottom: 12 }]}
-          >
-            <TextInput
-              placeholder="Password"
-              placeholderTextColor={"gray"}
-              value={password}
-              onChangeText={setPassword}
-              secureTextEntry={true}
-            />
-          </Animated.View>
-          <Animated.View
-            entering={FadeInDown.duration(1000).delay(600).springify()}
-            style={styles.loginButtonContainer}
-          >
-            <TouchableOpacity
-              style={styles.loginButton}
-              onPress={() => HandleLogin()}
+        <Animated.View
+          style={styles.formCard}
+          entering={FadeInDown.duration(1000).springify()}
+        >
+          <View style={styles.formTitle}>
+            <Animated.Text
+              entering={FadeInUp.duration(1000).delay(200).springify()}
+              style={styles.title}
             >
-              <Text style={styles.loginButtonText}>Login</Text>
-            </TouchableOpacity>
-          </Animated.View>
-        </View>
+              Gaming Space
+            </Animated.Text>
+          </View>
+          <View style={styles.formInputContainer}>
+            <Animated.View
+              entering={FadeInDown.duration(1000).delay(200).springify()}
+              style={styles.formInput}
+            >
+              <TextInput
+                placeholder="Email"
+                placeholderTextColor={"gray"}
+                value={email}
+                onChangeText={setEmail}
+              />
+            </Animated.View>
+            <Animated.View
+              entering={FadeInDown.duration(1000).delay(400).springify()}
+              style={[styles.formInput, { marginBottom: 12 }]}
+            >
+              <TextInput
+                placeholder="Password"
+                placeholderTextColor={"gray"}
+                value={password}
+                onChangeText={setPassword}
+                secureTextEntry={true}
+              />
+            </Animated.View>
+            <Animated.View
+              entering={FadeInDown.duration(1000).delay(600).springify()}
+              style={styles.loginButtonContainer}
+            >
+              <TouchableOpacity
+                style={styles.loginButton}
+                onPress={() => HandleLogin()}
+              >
+                <Text style={styles.loginButtonText}>Login</Text>
+              </TouchableOpacity>
+            </Animated.View>
+          </View>
+          <View style={styles.newAccount}>
+            <Animated.View
+              entering={FadeInDown.duration(1000).delay(800).springify()}
+            >
+              <Text style={{ color: "white", marginRight: 10, fontSize: 16 }}>
+                No account ?
+              </Text>
+            </Animated.View>
+            <Animated.View
+              entering={FadeInDown.duration(1000).delay(800).springify()}
+            >
+              <TouchableOpacity>
+                <Text
+                  style={{ color: "#51abcb", marginRight: 10, fontSize: 16 }}
+                >
+                  Create One !
+                </Text>
+              </TouchableOpacity>
+            </Animated.View>
+          </View>
+        </Animated.View>
       </View>
     </ScrollView>
   );
@@ -98,46 +136,32 @@ export default function LoginScreen() {
 
 const styles = {
   container: {
-    backgroundColor: "#fff",
+    backgroundColor: "#FFF",
     height: "100%",
     width: "100%",
-  },
-  bgImage: {
-    width: "100%",
-    height: 850,
-    position: "absolute",
-  },
-  lampContainer: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-    width: "100%",
-    height: "100%",
-    position: "absolute",
-  },
-  lamp1: {
-    height: 250,
-    width: 100,
-  },
-  lamp2: {
-    height: 175,
-    width: 70,
   },
   formContainer: {
-    height: "100%",
-    width: "100%",
     justifyContent: "space-around",
-    paddingTop: 290,
-    paddingBottom: 40,
+    paddingTop: 350,
+  },
+  formCard: {
+    backgroundColor: "#1a6985",
+    borderRadius: 16,
+    paddingTop: 20,
+    paddingBottom: 20,
+    marginHorizontal: 20,
+    elevation: 10,
+    marginBottom: 20,
   },
   formTitle: {
     alignItems: "center",
-    marginBottom: 60,
+    marginBottom: 30,
   },
   title: {
     color: "#fff",
     fontWeight: "700",
     letterSpacing: 0.05,
-    fontSize: 48,
+    fontSize: 44,
     lineHeight: 56,
   },
   formInputContainer: {
@@ -168,5 +192,12 @@ const styles = {
     fontWeight: "700",
     fontSize: 20,
     lineHeight: 28,
+  },
+  newAccount: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 20,
+    marginTop: 20,
   },
 };
